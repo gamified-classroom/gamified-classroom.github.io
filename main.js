1907bdfe1e99a7a6d4c1d9b41761df693d750237
arozoid <83493465+Arozoid@users.noreply.github.com>
2024-12-11 03:19:58 +0000

    a test to see if something works

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
 function handleCredentialResponse(response) {
     const idToken = response.credential;
   
-    gapi.load('auth2', () => {
-      gapi.auth2.init({
-        client_id: '296235127120-764s1kc7t7ocjcvbc3m79u8kgfs06or9.apps.googleusercontent.com',
-        scope: 'profile'
-      }).then(() => {
-        gapi.auth2.getAuthInstance().signIn().then(() => {
-          gapi.client.load('oauth2', 'v2').then(() => {
-            gapi.client.oauth2.userinfo.v2.me.get().execute(function(resp) {
-              if (resp.error) {
-                console.error('Error in retrieving user info:', resp.error);
-              } else {
-                const profilePictureUrl = resp.picture;
-                const profileImage = document.createElement('img');
-                profileImage.src = profilePictureUrl;
-                document.body.appendChild(profileImage);
-              }
-            });
-          });
-        });
+    // Remove gapi.auth2 initialization and sign-in
+    // Instead, directly use the idToken to fetch user info
+  
+    gapi.client.load('oauth2', 'v2').then(() => {
+      gapi.client.oauth2.userinfo.v2.me.get({
+        // Include the idToken in the request
+        oauth_token: idToken 
+      }).execute(function(resp) {
+        if (resp.error) {
+          console.error('Error in retrieving user info:', resp.error);
+        } else {
+          const profilePictureUrl = resp.picture;
+          const profileImage = document.createElement('img');
+          profileImage.src = profilePictureUrl;
+          document.body.appendChild(profileImage);
+        }
       });
     });
   }
