ebae7ea1a6b32417ba4a412d6e00355ef8a51191
arozoid <83493465+Arozoid@users.noreply.github.com>
2024-12-11 03:25:37 +0000

    fixing that something

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 function handleCredentialResponse(response) {
     const idToken = response.credential;
   
-    // Remove gapi.auth2 initialization and sign-in
-    // Instead, directly use the idToken to fetch user info
-  
     gapi.client.load('oauth2', 'v2').then(() => {
-      gapi.client.oauth2.userinfo.v2.me.get({
-        // Include the idToken in the request
-        oauth_token: idToken 
-      }).execute(function(resp) {
-        if (resp.error) {
-          console.error('Error in retrieving user info:', resp.error);
-        } else {
-          const profilePictureUrl = resp.picture;
-          const profileImage = document.createElement('img');
-          profileImage.src = profilePictureUrl;
-          document.body.appendChild(profileImage);
-        }
-      });
+      gapi.client.oauth2.userinfo.v2.me.get()
+        .execute(
+          function(resp) {
+            if (resp.error) {
+              console.error('Error in retrieving user info:', resp.error);
+            } else {
+              const profilePictureUrl = resp.picture;
+              const profileImage = document.createElement('img');
+              profileImage.src = profilePictureUrl;
+              document.body.appendChild(profileImage);
+            }
+          },
+          { // This object is for additional parameters
+            headers: {
+              'Authorization': `Bearer ${idToken}` 
+            }
+          }
+        );
     });
   }
