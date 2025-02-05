56d665a261b07083d274953e858c4eb4730b1f7a
arozoid <83493465+Arozoid@users.noreply.github.com>
2025-02-05 03:12:58 +0000

    bop

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const supabaseUrl = 'https://isyodevlgpglibltgyng.supabase.co'; // Replace with your Supabase URL
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzeW9kZXZsZ3BnbGlibHRneW5nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg1NjQ0MzcsImV4cCI6MjA1NDE0MDQzN30.JOocGXpDTmGaHZh33OQ4BBnNrKFEKtQmBJTDVTB3tFo'; // Replace with your Supabase anon key
+
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
 function handleCredentialResponse(response) {
     const idToken = response.credential;
   
