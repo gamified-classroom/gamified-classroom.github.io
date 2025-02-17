85d367027685313d3567e06c2632280593ddc9b0
arozoid <83493465+Arozoid@users.noreply.github.com>
2025-02-17 06:36:30 +0000

    pow

--- main.js
+++ main.js
@@ -1,36 +1,40 @@
-const supabaseUrl = 'https://isyodevlgpglibltgyng.supabase.co'; // Replace with your Supabase URL
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzeW9kZXZsZ3BnbGlibHRneW5nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg1NjQ0MzcsImV4cCI6MjA1NDE0MDQzN30.JOocGXpDTmGaHZh33OQ4BBnNrKFEKtQmBJTDVTB3tFo'; // Replace with your Supabase anon key
+const supabaseUrl = 'https://isyodevlgpglibltgyng.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzeW9kZXZsZ3BnbGlibHRneW5nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg1NjQ0MzcsImV4cCI6MjA1NDE0MDQzN30.JOocGXpDTmGaHZh33OQ4BBnNrKFEKtQmBJTDVTB3tFo'
 
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+const database = supabase.createClient(supabaseUrl, supabaseKey)
+console.log('Supabase Instance: ', database)
 
 document.addEventListener('DOMContentLoaded', function() {
     function handleCredentialResponse(response) {
-        console.log("Encoded JWT ID token: " + response.credential);
+        console.log("Encoded JWT ID token: " + response.credential)
         try { // Add a try-catch block
-            const decodedToken = jwt_decode(response.credential);
-            alert("Decoded JWT ID token:", decodedToken);
-            console.log("User email:", decodedToken.email);
-            console.log("User name:", decodedToken.name);
-            console.log("User picture:", decodedToken.picture);
+            const decodedToken = jwt_decode(response.credential)
+            alert("Decoded JWT ID token:", decodedToken)
+            console.log("User email:", decodedToken.email)
+            console.log("User name:", decodedToken.name)
+            console.log("User picture:", decodedToken.picture)
         } catch (error) {
-            console.error("Error decoding JWT:", error);
-            console.log("Response:", response); // Log the full response for debugging
+            console.error("Error decoding JWT:", error)
+            console.log("Response:", response) // Log the full response for debugging
         }
     }
 
     try { // Try-catch for GSI initialization
         google.accounts.id.initialize({
-            client_id: '296235127120-764s1kc7t7ocjcvbc3m79u8kgfs06or9.apps.googleusercontent.com', // Replace with your client ID
+            client_id: '296235127120-764s1kc7t7ocjcvbc3m79u8kgfs06or9.apps.googleusercontent.com',
             callback: handleCredentialResponse
-        });
+        })
 
         google.accounts.id.renderButton(
             document.getElementById("signInDiv"), {
                 theme: "outline",
                 size: "large"
             }
-        );
+        )
+
+        google.accounts.id.prompt();
+        
     } catch (error) {
-        console.error("Error initializing or rendering GSI:", error);
+        console.error("Error initializing or rendering GSI:", error)
     }
-});
+})
